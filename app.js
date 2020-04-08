16adc79d357470c0ec0bbf233b6a7dd4fa279b71
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-04-08 23:34:31 +0600

    post req

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-
 const port = 8080;
 
+app.use(express.json()); //otherwise post request won't work
+
+//server initialization
 app.listen(port, () => {
    console.log(`Listening to port ${port}`);
 });
 
+
+//'tours-simple.json' is working as a database
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+
+
+//get all tours
 app.get('/api/v1/tours', (req, res) => {
    res.status(200).json({
       status: 'success',
@@ -20,3 +27,22 @@ app.get('/api/v1/tours', (req, res) => {
    })
 });
 
+
+
+//create new tours
+app.post('/api/v1/tours', (req, res) => {
+   const newId = tours[tours.length - 1].id + 1; //create new id
+   const newTour = Object.assign({ id: newId }, req.body); //new tour object
+
+   tours.push(newTour); //add with the exiting ones
+
+   //update database
+   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+      res.status(201).json({
+         status: 'success',
+         data: {
+            tour: newTour
+         }
+      });
+   });
+});
