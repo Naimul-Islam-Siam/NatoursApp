61a33b7afd263f79ec80584c3e2784220fb4b692
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-04-30 03:33:51 +0600

    update refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -90,18 +90,28 @@ exports.createTour = async (req, res) => {
       res.status(400).json({
          status: "fail",
          message: error
-      })
+      });
    }
 };
 
 
-exports.updateTour = (req, res) => {
-   res.status(200).json({
-      status: 'success',
-      data: {
-         tour: `<Updated Tour>`
-      }
-   });
+exports.updateTour = async (req, res) => {
+   try {
+      const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+      // runValidators enforce model schema, so price can't be anything other than number, otherwise it'll cause an error
+
+      res.status(200).json({
+         status: 'success',
+         data: {
+            tour // same as, tour: tour
+         }
+      });
+   } catch (error) {
+      res.status(400).json({
+         status: "fail",
+         message: error
+      });
+   }
 };
 
 
