5cf7ceb51128c15f96cbd80858e62c2c5ff0ada5
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-04-30 05:17:18 +0600

    delete refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -115,10 +115,19 @@ exports.updateTour = async (req, res) => {
 };
 
 
-exports.deleteTour = (req, res) => {
-   // status code for delete is 204
-   res.status(204).json({
-      status: 'success',
-      data: null
-   });
+exports.deleteTour = async (req, res) => {
+   try {
+      await Tour.findByIdAndDelete(req.params.id, req.body);
+
+      // status code for delete is 204
+      res.status(204).json({
+         status: 'success',
+         data: null
+      });
+   } catch (error) {
+      res.status(400).json({
+         status: "fail",
+         message: error
+      });
+   }
 };
