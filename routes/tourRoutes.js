3525765c554ce9d78a8f556ebbf148b09158afbd
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-05-03 04:35:37 +0600

    aliasing api

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController'); // route handlers
-const { getAllTours, getIndividualTour, createTour, updateTour, deleteTour } = tourController;
+const { getAllTours, getIndividualTour, createTour, updateTour, deleteTour, aliasTopTours } = tourController;
 
 
 const router = express.Router(); // router middleware; express.Router() is a middleware
 
 // param middleware
 // router.param('id', checkID);
+
+router
+   .route('/top-5-cheap')
+   .get(aliasTopTours, getAllTours); // aliasTopTours will first auto prefill the query and then call getAllTours
+
 
 router
    .route('/')
@@ -19,7 +24,6 @@ router
    .get(getIndividualTour)
    .patch(updateTour)
    .delete(deleteTour);
-
 
 
 
