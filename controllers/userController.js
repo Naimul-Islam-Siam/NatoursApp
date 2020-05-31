ff8c2913dde121263c3b7275af043d7867c15b4f
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-05-31 23:00:34 +0600

    updateMe middleware which lets user update their name and email but not password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,45 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+
+const filterObj = (obj, ...allowedFields) => {
+   const newObj = {};
+
+   Object.keys(obj).forEach(el => {
+      if (allowedFields.includes(el)) {
+         newObj[el] = obj[el];
+      }
+   });
+
+   return newObj;
+};
+
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+   // 1) create error if user tries to update password
+   if (req.body.password || req.body.passwordConfirm) {
+      return next(new AppError(`This route is not for updating passwords!! Please use /updatePassword for that.`, 400));
+   }
+
+   // 2) filter out unwanted field names that are not allowed to be updated
+   const filteredBody = filterObj(req.body, 'name', 'email');
+
+   // 3) if not the above, update user document
+   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+      new: true,
+      runValidators: true
+   });
+
+   res.status(200).json({
+      status: 'success',
+      data: {
+         user: updatedUser
+      }
+   });
+});
+
+
 
 //============================
 // Route handler functions
