cb918d3b6a6c503707056753c8f4d7fc5eb2cc17
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-06 03:17:06 +0600

    add import and delete options

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' }); // configure dotenv file
 
@@ -23,11 +25,16 @@ mongoose.connect(DB, {
 
 // read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 // import data to DB
 const importData = async () => {
    try {
       await Tour.create(tours);
+      await User.create(users);
+      await Review.create(reviews);
+
       console.log("Data successfully loaded");
    } catch (error) {
       console.log(error);
@@ -39,6 +46,9 @@ const importData = async () => {
 const deleteData = async () => {
    try {
       await Tour.deleteMany();
+      await User.deleteMany();
+      await Review.deleteMany();
+
       console.log("Data successfully deleted");
    } catch (error) {
       console.log(error);
