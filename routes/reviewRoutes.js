6ee09284f8d35da87daaaefa0042e120f85750e8
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-05 15:49:01 +0600

    add new routes for reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
-const { createReview, getAllReviews } = reviewController;
 const { protect, restrictTo } = authController;
+const { setTourAndUserIds, getIndividualReview, createReview, getAllReviews, updateReview, deleteReview } = reviewController;
 
 const router = express.Router({ mergeParams: true }); // to get access to `:tourId` param from tourRoutes
 
 // POST  /tours/:tourId/reviews
 // GET  /tours/:tourId/reviews
 
-// when thes url appears, first it will be redirected to tourRoutes then from there it'll be redirected to reviewRoutes
+// when these url appears, first it will be redirected to tourRoutes then from there it'll be redirected to reviewRoutes
 // mergeParams help with that
 
 router
    .route('/')
    .get(getAllReviews)
-   .post(protect, restrictTo('user'), createReview);
+   .post(protect, restrictTo('user'), setTourAndUserIds, createReview);
+
+router
+   .route('/:id')
+   .get(getIndividualReview)
+   .patch(updateReview)
+   .delete(deleteReview);
+
 
 module.exports = router;
