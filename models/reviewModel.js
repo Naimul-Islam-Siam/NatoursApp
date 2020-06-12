62bf74ff333fb76674299be4e83c6ba4cc11aa81
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-12 22:51:40 +0600

    number of ratings and average rating of a tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
    review: {
@@ -39,6 +40,38 @@ reviewSchema.pre(/^find/, function (next) {
    // didn't populate tour to avoid populate chain which can cause performance issues
 
    next();
+});
+
+
+// calculates the number of ratings and average rating of a particular tour
+// used statics to use aggregate so that `this` points to the model while using `this.aggregate`
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+   const stats = await this.aggregate([
+      {
+         $match: { tour: tourId }
+      },
+      {
+         $group: {
+            _id: '$tour',
+            numRatings: { $sum: 1 },
+            avgRating: { $avg: '$rating' }
+         }
+      }
+   ]);
+
+   // update the tour with new stats and save
+   await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: stats[0].avgRating,
+      ratingsQuantity: stats[0].numRatings
+   });
+};
+
+
+reviewSchema.post('save', function () {
+   // `this` points to the current document(current tour)\
+   // `this.constructor` points to current Model (We can't use Review.calcAverageRatings here, as `Review` is created after this middleware)
+
+   this.constructor.calcAverageRatings(this.tour);
 });
 
 
