5f08d97c8a4cea42b7a94355bfd5c51614c3bef0
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-14 02:56:07 +0600

    getDistances middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -223,3 +223,45 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
       }
    });
 });
+
+
+// calculate the distances of start locations of all the tours from a particular place
+// /distances/:latlng/unit/:unit
+exports.getDistances = catchAsync(async (req, res, next) => {
+   const { latlng, unit } = req.params;
+   const [lat, lng] = latlng.split(',');
+
+   const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+   if (!lat || !lng) {
+      next(new AppError(`Please provide latitude and longitude in the form lat,lng`, 400));
+   }
+
+   const distances = await Tour.aggregate([
+      {
+         $geoNear: {
+            // from where distances will be calculated
+            near: {
+               type: 'Point',
+               coordinates: [lng * 1, lat * 1]
+            },
+            distanceField: 'distance',
+            distanceMultiplier: multiplier // converts to mi or km
+         }
+      },
+      {
+         $project: {
+            // only show these fields
+            name: 1,
+            distance: 1
+         }
+      }
+   ]);
+
+   res.status(200).json({
+      status: 'success',
+      data: {
+         data: distances
+      }
+   });
+});
