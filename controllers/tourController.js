4572efbf8b34f080e24b4e1420d30b8edec2fea8
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-14 00:23:32 +0600

    getToursWithin middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const Tour = require('./../models/tourModel');
 // const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
-// const AppError = require('./../utils/appError');
+const AppError = require('./../utils/appError');
 const handlerFactory = require('./handlerFactory');
 
 // custom middleware
@@ -189,6 +189,37 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
       status: 'success',
       data: {
          plan
+      }
+   });
+});
+
+
+// /tour-within/:distance/center/:latlng/unit/:unit
+// /tour-within/300/center/34.111745,-118.113491/unit/mi
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+   const { distance, latlng, unit } = req.params;
+   const [lat, lng] = latlng.split(',');
+   const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+   if (!lat || !lng) {
+      next(new AppError(`Please provide latitude and longitude in the form lat,lng`, 400));
+   }
+
+   const tours = await Tour.find({
+      startLocation: {
+         $geoWithin: {
+            $centerSphere: [
+               [lng, lat], radius
+            ]
+         }
+      }
+   });
+
+   res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+         data: tours
       }
    });
 });
