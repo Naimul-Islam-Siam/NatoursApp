23f65f22227cb5da7a9f60a9bb4ba2d8181c8345
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-19 03:54:32 +0600

    getTour middleware

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -8,15 +8,24 @@ exports.getOverview = catchAsync(async (req, res, next) => {
    // 2) Build template
 
    // 3) Render template with the tour data
-
    res.status(200).render('overview', {
       title: "All Tours",
       tours
    });
 });
 
-exports.getTour = (req, res) => {
-   res.status(200).render('tour', {
-      title: "The Forest Hiker Tour"
+exports.getTour = catchAsync(async (req, res) => {
+   // 1) Get the data of the requested tour (including guides and reviews)
+   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+      path: 'reviews',
+      fields: 'review rating user'
    });
-};
+
+   // 2) Build template
+
+   // 3) Render template with the data
+   res.status(200).render('tour', {
+      title: tour.name,
+      tour
+   });
+});
