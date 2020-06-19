a0dfcbeffe34adea0582be47bf0c23eac3494026
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-19 23:27:41 +0600

    login

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -14,7 +14,8 @@ exports.getOverview = catchAsync(async (req, res, next) => {
    });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+
+exports.getTour = catchAsync(async (req, res, next) => {
    // 1) Get the data of the requested tour (including guides and reviews)
    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
       path: 'reviews',
@@ -27,5 +28,12 @@ exports.getTour = catchAsync(async (req, res) => {
    res.status(200).render('tour', {
       title: tour.name,
       tour
+   });
+});
+
+
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+   res.status(200).render('login', {
+      title: 'Login to your account'
    });
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const viewController = require('./../controllers/viewController');
-const { getOverview, getTour } = viewController;
+const { getOverview, getTour, getLoginForm } = viewController;
 
 
-router.get('/', getOverview);
+router
+   .get('/', getOverview);
 
-router.get('/tour/:slug', getTour);
+router
+   .get('/tour/:slug', getTour);
+
+router
+   .get('/login', getLoginForm);
 
 module.exports = router;
