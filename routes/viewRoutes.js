2caa6b2e9af6bd8091aa05518a49ead9bba3ff52
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-20 00:00:11 +0600

    login

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const viewController = require('./../controllers/viewController');
+const authController = require('./../controllers/authController');
 const { getOverview, getTour, getLoginForm } = viewController;
+const { protect } = authController;
 
 
 router
    .get('/', getOverview);
 
 router
-   .get('/tour/:slug', getTour);
+   .get('/tour/:slug', protect, getTour);
 
 router
    .get('/login', getLoginForm);
