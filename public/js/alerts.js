384b914ed5e0e17ff4d61cb26128b3d9aa7af922
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-24 22:52:32 +0600

    login alerts

---
+++ public/js/alerts.js
@@ -0,0 +1,16 @@
+export const showAlert = (type, message) => {
+   hideAlert();
+   // type is either 'success' or 'error'
+   const markup = `<div class="alert alert--${type}">${message}</div>`;
+
+   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+
+   window.setTimeout(hideAlert, 5000);
+};
+
+export const hideAlert = () => {
+   const element = document.querySelector('.alert');
+   if (element) {
+      element.parentElement.removeChild(element);
+   }
+};
