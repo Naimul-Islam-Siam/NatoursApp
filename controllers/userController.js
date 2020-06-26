f439417ad030d3a77ae3b510292acf73386102fd
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-06-26 23:50:16 +0600

    resize image, image processing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,22 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const handlerFactory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-   destination: (req, file, cb) => {
-      cb(null, 'public/img/users');
-   },
-   filename: (req, file, cb) => {
-      //filename will be: user-userid-currenttimestamp.extension
-      const extension = req.file.mimetype.split('/')[1];
+// const multerStorage = multer.diskStorage({
+//    destination: (req, file, cb) => {
+//       cb(null, 'public/img/users');
+//    },
+//    filename: (req, file, cb) => {
+//       //filename will be: user-userid-currenttimestamp.extension
+//       const extension = req.file.mimetype.split('/')[1];
 
-      cb(null, `user-${req.user.id}-${Date.now()}.${extension}`);
-   }
-});
+//       cb(null, `user-${req.user.id}-${Date.now()}.${extension}`);
+//    }
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
    if (req.file.mimetype.startsWith('image')) {
@@ -31,6 +33,21 @@ const upload = multer({
 
 
 exports.uploadUserPhoto = upload.single('photo');
+
+
+exports.resizeUserPhoto = (req, res, next) => {
+   if (!req.file) return next();
+
+   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+   sharp(req.file.buffer)
+      .resize(500, 500)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/img/users/${req.file.filename}`);
+
+   next();
+};
 
 
 const filterObj = (obj, ...allowedFields) => {
@@ -61,6 +78,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
    // 2) filter out unwanted field names that are not allowed to be updated
    const filteredBody = filterObj(req.body, 'name', 'email');
+
+   // photo update
+   if (req.file) {
+      filteredBody.photo = req.file.filename;
+   }
 
    // 3) Check if the newly provided data is already the existing one
    const user = await User.findById(req.user.id);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userController = require('./../controllers/userController'); // route handlers
 const authController = require('./../controllers/authController');
-const { uploadUserPhoto, getMe, getAllUsers, getIndividualUser, createUser, updateUser, deleteUser, updateMe, deactivateMe } = userController;
+const { getMe, getAllUsers, getIndividualUser, createUser, updateUser, deleteUser, updateMe, deactivateMe } = userController;
 const { signup, accountConfirm, login, logout, protect, restrictTo, forgotPassword, resetPassword, updatePassword } = authController;
 
 
@@ -33,7 +33,7 @@ router
    .get('/me', protect, getMe, getIndividualUser);
 
 router
-   .patch('/updateMe', protect, uploadUserPhoto, updateMe);
+   .patch('/updateMe', protect, userController.uploadUserPhoto, userController.resizeUserPhoto, updateMe);
 
 router
    .delete('/deactivateMe', protect, deactivateMe);
