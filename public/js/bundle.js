84b9312be033db3d458d67cf0c448931f623b565
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-07-11 03:45:24 +0600

    js bundle

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9609,7 +9609,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3024" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8055" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
